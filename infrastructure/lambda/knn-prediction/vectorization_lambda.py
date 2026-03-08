3f527a1e225db99ce149f5aae5c39e809a497f26
Nabhan <nabedi2006@gmail.com>
2026-03-08 00:52:33 -0500

    created vectorization lambda

---
+++ infrastructure/lambda/knn-prediction/vectorization_lambda.py
@@ -0,0 +1,56 @@
+import json
+import joblib
+import boto3
+import io
+import os
+import pandas as pd
+from datetime import datetime
+
+
+s3 = boto3.client('s3')
+
+
+def load_scalar():
+    bucket = os.environ['S3_BUCKET_NAME']
+    key = 'models/car_scaler.joblib'
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+    scaler_bytes = response['Body'].read()
+    return joblib.load(io.BytesIO(scaler_bytes))
+
+
+def event_handler(event, context):
+    try:
+        brand = event.get('InputBrand')
+        model = event.get('InputModel')
+        year = int(event.get('InputYear'))
+        condition = int(event.get('InputConditionScore'))
+        kilometers = int(event.get('InputKilometers'))
+        fuel_type = event.get('InputFuelType', '').upper()
+
+        current_year = datetime.now().year
+        age = current_year - year
+
+        is_petrol = 1 if 'PETROL' in fuel_type else 0
+        is_diesel = 1 if 'DIESEL' in fuel_type else 0
+
+        scalar = load_scalar()
+
+        numerical_features = [[age, kilometers, condition, 1]]
+        scaled_nums = scalar.transform(numerical_features)[0]
+
+        final_vector = [float(x) for x in scaled_nums] + [is_petrol, is_diesel, 0, 0]
+
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'vector': final_vector
+            })
+        }
+    
+    except Exception as e:
+       return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
