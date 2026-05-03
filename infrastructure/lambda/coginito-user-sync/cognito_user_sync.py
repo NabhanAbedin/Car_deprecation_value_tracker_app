bfbcaff041ad528c29c696bc40c11692ebbc0802
Nabhan <nabedi2006@gmail.com>
2026-05-03 11:32:45 -0400

    updated lambda

--- infrastructure/lambda/coginito-user-sync/cognito_user_sync.py
+++ infrastructure/lambda/coginito-user-sync/cognito_user_sync.py
@@ -14,6 +14,7 @@ def handler(event,context):
         host = os.environ['DB_HOST'],
         port = os.environ.get('DB_PORT',5432),
         dbname = os.environ['DB_NAME'],
+        user = os.environ['DB_USER'],
         password = os.environ['DB_PASSWORD']
     )
 
